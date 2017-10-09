a389feda2ad4a7028fe164dceae5ba126caeb179
沈洋 <sy109491@alibaba-inc.com>
2017-10-09 18:22:41 +0800

    some more script

--- script/init.lua
+++ script/init.lua
@@ -1,13 +1,23 @@
 start='emmm...., think a lot and decide to write this,maybe it is dramaSprite, for content choose not on holiday hope least bother'
-apologize = {'maybe it is dramaSprite','','','dramaSprite actions at old time'}
-doNext = {GravityFall}
+apologize = {'maybe it is dramaSprite','han','m','dramaSprite actions at old time'}
+doNext = {memory,GravityFall,song}
 
-middle={noMoreEmbarrassed,importance,myFeature, yourFeature,houseStuff}
+middle={noMoreEmbarrassed,abitFear,importance,myFeature, yourFeature,houseStuff}
 noEmbarrassed={}
 importance={}
 yourFeature={}
 myFeature={}
 houseStuff={}
+
+middle = map(issues)
+
+issues={
+    
+    myFeature:
+    houseStuff:
+    expetation:
+    memory:
+}
 
 local passage = {start,apologize,middle,{houseStuff|yourFeature,doNext}}
 
